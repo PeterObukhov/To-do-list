f24a25d4f9eeced29001008494d3b731ed4e0efa
PeterObukhov <petr.obuhov.2002@mail.ru>
2023-12-25 00:58:48 +0300

    Add project files.

---
+++ To do list/Task.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace To_do_list
+{
+    internal class Task : INotifyPropertyChanged
+    {
+        private string description;
+        private bool isCompleted;
+
+        public string Description 
+        {
+            get
+            {
+                return description;
+            }
+            set 
+            {
+                description = value;
+                OnPropertyChanged("Description");
+            }
+        }
+        public bool IsCompleted
+        {
+            get
+            {
+                return isCompleted;
+            }
+            set
+            {
+                isCompleted = value;
+                OnPropertyChanged("IsCompleted");
+            }
+        }
+        public List<Task> Subtasks { get; set; }
+        public DateTime Deadline { get; set; }
+        public bool HasSubtasks
+        {
+            get { return Subtasks != null && Subtasks.Count > 0; }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
+        public void ChangeState()
+        {
+            IsCompleted = !IsCompleted;
+        }
+    }
+}
---
+++ To do list/TaskList.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Printing;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+
+namespace To_do_list
+{
+    internal class TaskList : INotifyPropertyChanged
+    {
+        //Task selectedTask
+        //{
+        //    get { return selectedTask; }
+        //    set 
+        //    { 
+        //        selectedTask = value;
+        //        OnPropertyChanged("SelectedTask");
+        //    }
+        //}
+        public ObservableCollection<Task> Tasks { get; set; }
+        public int Count
+        {
+            get { return Tasks.Count; }
+        }
+
+        public void Delete(int id)
+        {
+            Tasks.RemoveAt(id);
+        }
+
+        public TaskList()
+        {
+            Tasks = new ObservableCollection<Task>() 
+            { 
+                new Task() 
+                { 
+                    Description = "Задача 1", 
+                    Subtasks = new List<Task>()
+                    { 
+                        new Task() { Description = "Подзадача 1" },
+                        new Task() { Description = "Подзадача 2" }
+                    } 
+                },
+                new Task() { Description = "Задача 2" },
+            };
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
