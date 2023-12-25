bc500d2357d21bf771f177c766cc0e8c57b2b84c
Петр Обухов <petr.obuhov.2002@mail.ru>
2023-12-25 15:29:25 +0300

    added TaskView control

--- To do list/TaskView.xaml.cs
+++ To do list/TaskView.xaml.cs
@@ -20,9 +20,30 @@ namespace To_do_list
     /// </summary>
     public partial class TaskView : UserControl
     {
+        public static readonly DependencyProperty DescriptionProperty =
+            DependencyProperty.Register("Description", typeof(int), typeof(UserControl), new FrameworkPropertyMetadata(null));
+
         public TaskView()
         {
             InitializeComponent();
+        }
+
+        public string Description
+        {
+            get
+            {
+                return (string)GetValue(DescriptionProperty);
+            }
+            set
+            {
+                SetValue(DescriptionProperty, value);
+            }
+        }
+
+        private void ToggleStrikethrough(object sender, RoutedEventArgs e)
+        {
+            // Set the underline decoration directly to the text block.
+            DescriptionBlock.TextDecorations = DescriptionBlock.TextDecorations == TextDecorations.Strikethrough ? null : TextDecorations.Strikethrough;
         }
     }
 }
